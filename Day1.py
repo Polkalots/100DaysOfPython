04d78b3314d1a2d086da64ff5674ffcd8b841538
Polkalots <polkalots1@gmail.com>
2026-01-23 14:02:58 -0700

    Added working code for Day1 Challenge

--- Day1.py
+++ Day1.py
@@ -1,1 +1,13 @@
+#Task: Write a script that reads a text file and prints the number of lines.
+
+def get_lines(file):
+    with open(file, 'r') as text_file:
+        lines = 0
+        for line in text_file:
+            lines += 1
+        return lines
+    
+print(get_lines(r"[file path]"))
+
+
 
