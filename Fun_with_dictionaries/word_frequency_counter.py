e4acebf11e653cce7c1d031fe98ff5558a9a36ba
Polkalots <polkalots1@gmail.com>
2026-02-19 07:53:35 -0700

    Practice working with dictionaries in Python.

---
+++ Fun_with_dictionaries/word_frequency_counter.py
@@ -0,0 +1,33 @@
+import argparse
+import re
+from collections import Counter
+
+def main(file, word=None):
+    word_count = word_counter(file)
+    if word:
+        print(f'Searching for word {word} in {file}...')
+        if word in word_count:
+            print(f'{word} : {word_count.get(word)}')
+        else:
+            print(f'{word} not found in {file}.')
+    else:
+        print('Word Count: ')
+        for word in word_count:
+            print(f'{word} : {word_count.get(word)}')
+
+def word_counter(file):
+    with open(file, 'r') as f:
+        text = f.read().lower() 
+        words = re.findall(r"[a-z]+(?:'[a-z]+)?", text) #find all words, including words with apostrophes. This normalizes the words in the file, removing whitespace and punctuation.
+        return Counter(words) # Create a dictionary with each word and it's count for each word in the input file.
+    
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', required=True, help='Path to input file')
+    parser.add_argument('-w', '--word', help='Optional word to search for')
+    args = parser.parse_args()
+    main(args.file, args.word)
+
+# 2/19/2026
+# Working on practicing more with dictionaries. In this task, we built a slightly more robust command-line word counter.              
+               
