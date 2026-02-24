d8387398f3697d76df02020056c23f5b05198fa6
Polkalots <polkalots1@gmail.com>
2026-02-23 23:00:26 -0700

    Added function to search log entries by severity.

--- Fun_with_dictionaries/log_severity_aggregator.py
+++ Fun_with_dictionaries/log_severity_aggregator.py
@@ -3,9 +3,19 @@ import csv
 import argparse
 from collections import Counter
 
-def main(file):
-    log_file = count_log_levels(file)
-    #for line in log_file:
+def main(file, level=None):
+    level_count = count_log_levels(file)
+    level_lines = find_levels(file, level)
+    for l, c in level_count.items():
+        print(f"Level:\n{l}:{c}\n")
+
+    if level:
+        if level_lines:
+            for line in level_lines:
+                print(line)
+        else:
+            print('Level not found.')
+       #for line in log_file:
         #print(line)
 
 def count_log_levels(file):
@@ -20,13 +30,24 @@ def count_log_levels(file):
                 level_values[level] = 1
             else:
                 level_values[level] += 1
-    print(level_values)
-    #return log_file
+    return level_values
+
+def find_levels(file, level):
+    log_lines = []
+    levels = []
+    for item in level:
+        levels.append(item.strip().lower())
+    with open(file, newline='') as f:
+        log_file=csv.DictReader(f)
+        for line in log_file:
+            if line['Level'].lower().strip() in levels:
+                log_lines.append(line)
+        return log_lines
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', required=True)
-    parser.add_argument('-l', '--level', action='append', help='Accepts a case-sensitive string matching a log level. Options are "Error", "Info" and "Warning"')
+    parser.add_argument('-l', '--level', action='append', help='Accepts a case-sensitive string matching a log level. Options are "error", "notice" and "warning"')
     args = parser.parse_args()
 
     main(args.file, args.level)
@@ -34,3 +55,5 @@ if __name__=='__main__':
     # 2/21/2026: Getting some more practice working with dictionaries and csv files.
     # Today I started a script that will collect and count the different log levels from an apache log csv file.
     # I'll be adding more to it, but this is a start.
+
+    # 2/23/2026: Today I worked on adding functionality to search for and print specific log levels. 
